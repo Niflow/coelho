00a2a288134fdecc3fbe7451d7d7fb5e1291e517
Niflow <100972359+Niflow@users.noreply.github.com>
2022-08-12 21:02:09 -0300

    Continuar classe conexão

---
+++ conexao.js
@@ -0,0 +1,6 @@
+class Conexao {
+    constructor(objetoA,objetoB)
+    {
+        var ultimaconexao=
+    }
+}
--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ const Compostos = Matter.Composites;
 const Composto = Matter.Composite;
 
 var motorDeFisica, mundo;
-var chao, corda;
+var chao, corda, fruta;
 
 function setup() 
 {
@@ -18,6 +18,9 @@ function setup()
 
   chao = new Chao(200, 690, 600, 20);
   corda = new Corda(6, {x:245, y:30});
+  fruta = Objetos.circle(300,300,15,{density:0.001});
+  Composto.add(corda.body,fruta);
+  console.log(corda)
 
   rectMode(CENTER);
   ellipseMode(RADIUS);
@@ -28,7 +31,9 @@ function draw()
 {
   background(51);
   MotorFisica.update(motorDeFisica);
+  
 
   chao.mostrar();
   corda.show();
+  ellipse(fruta.position.x,fruta.position.y,15,15);
 }
