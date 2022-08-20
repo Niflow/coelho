e2c62d9cdbeab8b68fc813d645839ae3e48ba0b5
andressalessa <lessadasilveiraandressa@gmail.com>
2022-08-19 21:03:32 -0300

    começar aula C30

--- conexao.js
+++ conexao.js
@@ -1,6 +1,24 @@
 class Conexao {
     constructor(objetoA,objetoB)
     {
-        var ultimaconexao=
+        var ultimaConexao = objetoA.body.bodies.length - 2;
+        // var ultimaConexao = 1;
+        // tradução
+        // create = criar
+        // body = corpo
+        // bodies = corpos
+        // array = vetor = gavetas
+        this.conexao = Restricao.create(
+            {
+               bodyA: objetoA.body.bodies[ultimaConexao],
+               pointA: {x:0, y:0},
+               bodyB: objetoB,
+               pointB: {x:0, y:0},
+               length: -10,
+               stiffness: 0.01
+            }
+        );
+
+        Mundo.add(mundo, this.conexao);
     }
 }
--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ const Compostos = Matter.Composites;
 const Composto = Matter.Composite;
 
 var motorDeFisica, mundo;
-var chao, corda, fruta;
+var chao, corda, fruta, conexaoFruta;
 
 function setup() 
 {
@@ -16,11 +16,18 @@ function setup()
   motorDeFisica = MotorFisica.create();
   mundo = motorDeFisica.world;
 
+  // tradução
+  // new = novo
   chao = new Chao(200, 690, 600, 20);
+  // criando um objeto corda
   corda = new Corda(6, {x:245, y:30});
+  
   fruta = Objetos.circle(300,300,15,{density:0.001});
   Composto.add(corda.body,fruta);
-  console.log(corda)
+  
+  conexaoFruta = new Conexao(corda, fruta);
+
+  console.log(Corda);
 
   rectMode(CENTER);
   ellipseMode(RADIUS);
